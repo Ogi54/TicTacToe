cc04fca304b480304e9ff0d1f1638e88388377b9
Oguz <wi25b501@technikum-wien.at>
2026-05-22 20:48:07 +0200

    Implement TicTacToe #6

--- src/main/java/at/technikum/App.java
+++ src/main/java/at/technikum/App.java
@@ -1,17 +1,42 @@
 package at.technikum;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
 
         Board board = new Board();
-        char currenPlayer = 'X';
+        Scanner scanner = new Scanner(System.in);
 
-        board.makeMove(0, 0, currenPlayer);
-        currenPlayer = 'O';
-        board.makeMove(1, 1, currenPlayer);
-        currenPlayer = 'X';
-        board.makeMove(0, 1, currenPlayer);
+        char currentPlayer = 'X';
+
+        while (!board.isBoardFull()) {
+
+            System.out.println("Current player: " + currentPlayer);
+
+            System.out.println("Enter row:");
+            int row = scanner.nextInt();
+
+            System.out.println("Enter column:");
+            int col = scanner.nextInt();
+
+            board.makeMove(row, col, currentPlayer);
+
+            board.printBoard();
+
+            if (board.checkWinner(currentPlayer)) {
+                System.out.println("Player " + currentPlayer + " wins!");
+                break;
+            }
+
+            if (currentPlayer == 'X') {
+                currentPlayer = 'O';
+            }
+            else {
+                currentPlayer = 'X';
+            }
+        }
 
         board.makeMove(0,2,'X');
         board.makeMove(5, 5, 'X');
